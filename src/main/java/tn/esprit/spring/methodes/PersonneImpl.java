0c59e0da3721e465bde284f4ccccde3db46a0633
islemchatti99 <islem.zommitchatti@esprit.tn>
2022-03-22 12:31:09 +0100

    second commit

--- src/main/java/tn/esprit/spring/methodes/PersonneImpl.java
+++ src/main/java/tn/esprit/spring/methodes/PersonneImpl.java
@@ -4,6 +4,7 @@ import tn.esprit.spring.entities.Personne;
 public class PersonneImpl {
 
 	public void afficherPersonne (Personne P){
+		//*affichage *//
 		System.out.println("Le nom est : "+ P.getNom());
 		System.out.println("Le Prénom est : "+ P.getPrennom());
 	}
